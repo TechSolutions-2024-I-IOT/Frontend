fca6628bdcef291759c43dc2cdbee5ac45b8cd81
Sofia Alfaro <alfaros.sofia@gmail.com>
2024-06-22 01:58:45 -0500

    add: toolbar and routes

--- src/app/context/subscription/pages/pay-subscription/pay-subscription.component.ts
+++ src/app/context/subscription/pages/pay-subscription/pay-subscription.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { PaymentDetailComponent } from '../../components/payment-detail/payment-detail.component';
 import { SubscriptionPlanComponent } from '../../components/subscription-plan/subscription-plan.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-pay-subscription',
   standalone: true,
   imports: [
     PaymentDetailComponent,
-    SubscriptionPlanComponent
+    SubscriptionPlanComponent,
+    MatToolbarModule
   ],
   templateUrl: './pay-subscription.component.html',
   styleUrl: './pay-subscription.component.scss'
